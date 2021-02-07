1fe2f6a6cf3748e5b6de41606b5e97699198eb17
JeroenBoonen <76564930+JeroenBoonen@users.noreply.github.com>
2021-02-07 23:49:19 +0100

    Update playlists.js

--- src/playlists.js
+++ src/playlists.js
@@ -10,18 +10,8 @@ const clear = str => str
 
 export default [{
   playlistId: '6gHJB62EMpfvlR6IKAlE7V',
-  fetchTracks: () => fetchJson('http://www.n-joy.de/app/appplaylist100-app_station-njoy.json')
+  fetchTracks: () => fetchJson('http://icecast.vrtcdn.be/radio2_benebene.xspf')
     .then(({ content }) => content.map(({ artist, title }) =>
       clear(`${artist} ${title}`)))
     .then(tracks => tracks.map(track => ({ id: track, track }))),
-}, {
-  playlistId: '4RsocehVUYsvKz0QVZwHuc',
-  fetchTracks: () => fetchJson('http://www.sunshine-live.de/index.php?option=com_playhistory&task=current.track&format=json&stream=studio')
-    .then(({ data: { artist, title } }) => clear(`${artist} ${title}`))
-    .then(track => [{ id: track, track }]),
-}, {
-  playlistId: '3KqvELJ2mIqw8q1uSvTKhg',
-  fetchTracks: () => fetchJson('http://www.radio21.de/playlist/current_play/index.php')
-    .then(({ artist, song }) => clear(`${artist} ${song}`))
-    .then(track => [{ id: track, track }]),
 }]
